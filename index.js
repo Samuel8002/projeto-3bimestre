360427a7a95c58848b631c1b858c3e99bef0c3b8
Samuel8002 <rodriguescostasamuel16@gmail.com>
2025-08-14 12:01:00 -0300

    Henrique

--- index.js
+++ index.js
@@ -3,12 +3,44 @@ import express from 'express'
 const app = express()
 const PORT = 3000
 
+let usuarios = [];
+let contador = 1;
+
 app.use(express.json())
 
 //ROTA DE TESTE
 app.get("/status", (req, res) => {
   res.json({message: "API Online"})
 })
+
+//ROTA GET listar usuarios
+app.get("usuarios",(red,res)=>{
+  res.json(usuarios)
+})
+//ROTA POST criar novo usuario 
+app.post("/usuarios",(req,res) => {
+  const {nome} = red.body;
+
+  const novoUsuario = {
+    id: contador++,
+    nome
+  };
+
+  usuarios.push(novoUsuario);
+  res.status(201).json(novoUsuario);
+})
+
+//ROTA PUT atualiza usuarios por ID
+app.put("/usuarios/:id", (req,res) => {
+  const usuario = usuarios.find((i) => i.id === id);
+
+  if (!usuario) {
+    return res.status(404).json({ erro: "Usuario não encontrado"});
+  }
+
+  usuario.nome = nome;
+  res.json(usuario);
+});
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`)
